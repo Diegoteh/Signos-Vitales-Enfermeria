5a3485c44479b2025b2948b01516e9e6c0f7e337
Diegoteh <101595120+Diegoteh@users.noreply.github.com>
2026-08-30 16:17:27 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'signos-vitales-v2';
+const CACHE = 'signos-vitales-v3';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
